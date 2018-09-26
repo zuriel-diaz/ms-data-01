c1251375f4183c07ff694d7e5bc97aa986b57a95
Zuriel Diaz <zuriel.diaz@ibm.com>
2018-09-26 11:15:06 -0500

    fix: default constructor.

--- src/main/java/com/zlabs/msdata/Person.java
+++ src/main/java/com/zlabs/msdata/Person.java
@@ -16,6 +16,10 @@ public class Person {
 	private String lastName;
 	private String email;
 	
+	public Person() {
+		
+	}
+	
 	public Person(String firstName, String lastName, String email) {
 		this.firstName = firstName;
 		this.lastName = lastName;
