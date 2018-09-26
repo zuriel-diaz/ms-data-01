a03747f40b95611f3d51840dab87b7355ef56993
Zuriel Diaz <zuriel.diaz@ibm.com>
2018-09-26 10:58:45 -0500

    fix: URI and HTTP method [create Person].

--- src/main/java/com/zlabs/msdata/PersonController.java
+++ src/main/java/com/zlabs/msdata/PersonController.java
@@ -3,6 +3,7 @@ package com.zlabs.msdata;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class PersonController {
 	@Autowired
 	PersonRepository personRepository;
 	
+	@PostMapping("/people")
 	public Person store(@Valid @RequestBody Person person) {
 		return personRepository.save(person);
 	}
