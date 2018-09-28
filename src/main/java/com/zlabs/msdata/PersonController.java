8feeba3a0a9f662a546867af6390c69840ecde0c
Zuriel Diaz <zuriel.diaz@ibm.com>
2018-09-27 19:18:10 -0500

    CRUD: iteration 1.

--- src/main/java/com/zlabs/msdata/PersonController.java
+++ src/main/java/com/zlabs/msdata/PersonController.java
@@ -1,9 +1,15 @@
 package com.zlabs.msdata;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +25,30 @@ public class PersonController {
 	public Person store(@Valid @RequestBody Person person) {
 		return personRepository.save(person);
 	}
+	
+	@GetMapping("/people")
+	public List<Person> getPeople(){
+		return personRepository.findAll();
+	}
+	
+	@GetMapping("/people/{id}")
+	public Optional<Person> getPersonById(@PathVariable(value = "id") Long personId) {
+		return personRepository.findById(personId);
+	}
+	
+	@PutMapping("/people/{id}")
+	public Person update(@PathVariable(value = "id") Long personId, @Valid @RequestBody Person personData) {
+		Optional<Person> person = personRepository.findById(personId);
+		Person data = null;
+		
+		if (person != null && ) {
+			person.setFirstName( personData.getFirstName() );
+			person.setLastName( personData.getLastName() );
+			person.setEmail( personData.getEmail() );
+			data = personRepository.save(person);
+		}
+		return data ;
+	}
+	
 	
 }
